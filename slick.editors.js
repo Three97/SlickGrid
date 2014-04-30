a1b4224ea92c2e249d668f0006f60e3ada32cb2a
Jason Fedler <Jason.Fedler@gmail.com>
2014-04-30 14:14:10 -0400

    LongText editor with simple HTML wrapper
    Response to question
    https://github.com/mleibman/SlickGrid/issues/935

--- slick.editors.js
+++ slick.editors.js
@@ -15,7 +15,8 @@
         "YesNoSelect": YesNoSelectEditor,
         "Checkbox": CheckboxEditor,
         "PercentComplete": PercentCompleteEditor,
-        "LongText": LongTextEditor
+        "LongText": LongTextEditor,
+        "LongSimpleHtmlText": LongSimpleHtmlTextEditor
       }
     }
   });
@@ -408,105 +409,212 @@
   }
 
   /*
-   * An example of a "detached" editor.
-   * The UI is added onto document BODY and .position(), .show() and .hide() are implemented.
-   * KeyDown events are also handled to provide handling for Tab, Shift-Tab, Esc and Ctrl-Enter.
-   */
-  function LongTextEditor(args) {
-    var $input, $wrapper;
-    var defaultValue;
-    var scope = this;
+     * An example of a "detached" editor.
+     * The UI is added onto document BODY and .position(), .show() and .hide() are implemented.
+     * KeyDown events are also handled to provide handling for Tab, Shift-Tab, Esc and Ctrl-Enter.
+     */
+    function LongTextEditor(args) {
+        var $input, $wrapper;
+        var defaultValue;
+        var scope = this;
 
-    this.init = function () {
-      var $container = $("body");
+        this.init = function () {
+            var $container = $("body");
 
-      $wrapper = $("<DIV style='z-index:10000;position:absolute;background:white;padding:5px;border:3px solid gray; -moz-border-radius:10px; border-radius:10px;'/>")
-          .appendTo($container);
+            $wrapper = $("<DIV style='z-index:10000;position:absolute;background:white;padding:5px;border:3px solid gray; -moz-border-radius:10px; border-radius:10px;'/>")
+                .appendTo($container);
 
-      $input = $("<TEXTAREA hidefocus rows=5 style='backround:white;width:250px;height:80px;border:0;outline:0'>")
-          .appendTo($wrapper);
+            $input = $("<TEXTAREA hidefocus rows=5 style='backround:white;width:250px;height:80px;border:0;outline:0'>")
+                .appendTo($wrapper);
 
-      $("<DIV style='text-align:right'><BUTTON>Save</BUTTON><BUTTON>Cancel</BUTTON></DIV>")
-          .appendTo($wrapper);
+            $("<DIV style='text-align:right'><BUTTON>Save</BUTTON><BUTTON>Cancel</BUTTON></DIV>")
+                .appendTo($wrapper);
 
-      $wrapper.find("button:first").bind("click", this.save);
-      $wrapper.find("button:last").bind("click", this.cancel);
-      $input.bind("keydown", this.handleKeyDown);
+            $wrapper.find("button:first").bind("click", this.save);
+            $wrapper.find("button:last").bind("click", this.cancel);
+            $input.bind("keydown", this.handleKeyDown);
 
-      scope.position(args.position);
-      $input.focus().select();
-    };
+            scope.position(args.position);
+            $input.focus().select();
+        };
 
-    this.handleKeyDown = function (e) {
-      if (e.which == $.ui.keyCode.ENTER && e.ctrlKey) {
-        scope.save();
-      } else if (e.which == $.ui.keyCode.ESCAPE) {
-        e.preventDefault();
-        scope.cancel();
-      } else if (e.which == $.ui.keyCode.TAB && e.shiftKey) {
-        e.preventDefault();
-        args.grid.navigatePrev();
-      } else if (e.which == $.ui.keyCode.TAB) {
-        e.preventDefault();
-        args.grid.navigateNext();
-      }
-    };
+        this.handleKeyDown = function (e) {
+            if (e.which == $.ui.keyCode.ENTER && e.ctrlKey) {
+                scope.save();
+            } else if (e.which == $.ui.keyCode.ESCAPE) {
+                e.preventDefault();
+                scope.cancel();
+            } else if (e.which == $.ui.keyCode.TAB && e.shiftKey) {
+                e.preventDefault();
+                args.grid.navigatePrev();
+            } else if (e.which == $.ui.keyCode.TAB) {
+                e.preventDefault();
+                args.grid.navigateNext();
+            }
+        };
 
-    this.save = function () {
-      args.commitChanges();
-    };
+        this.save = function () {
+            args.commitChanges();
+        };
 
-    this.cancel = function () {
-      $input.val(defaultValue);
-      args.cancelChanges();
-    };
+        this.cancel = function () {
+            $input.val(defaultValue);
+            args.cancelChanges();
+        };
 
-    this.hide = function () {
-      $wrapper.hide();
-    };
+        this.hide = function () {
+            $wrapper.hide();
+        };
 
-    this.show = function () {
-      $wrapper.show();
-    };
+        this.show = function () {
+            $wrapper.show();
+        };
 
-    this.position = function (position) {
-      $wrapper
-          .css("top", position.top - 5)
-          .css("left", position.left - 5)
-    };
+        this.position = function (position) {
+            $wrapper
+                .css("top", position.top - 5)
+                .css("left", position.left - 5)
+        };
 
-    this.destroy = function () {
-      $wrapper.remove();
-    };
+        this.destroy = function () {
+            $wrapper.remove();
+        };
 
-    this.focus = function () {
-      $input.focus();
-    };
+        this.focus = function () {
+            $input.focus();
+        };
 
-    this.loadValue = function (item) {
-      $input.val(defaultValue = item[args.column.field]);
-      $input.select();
-    };
+        this.loadValue = function (item) {
+            $input.val(defaultValue = item[args.column.field]);
+            $input.select();
+        };
 
-    this.serializeValue = function () {
-      return $input.val();
-    };
+        this.serializeValue = function () {
+            return $input.val();
+        };
 
-    this.applyValue = function (item, state) {
-      item[args.column.field] = state;
-    };
+        this.applyValue = function (item, state) {
+            item[args.column.field] = state;
+        };
 
-    this.isValueChanged = function () {
-      return (!($input.val() == "" && defaultValue == null)) && ($input.val() != defaultValue);
-    };
+        this.isValueChanged = function () {
+            return (!($input.val() == "" && defaultValue == null)) && ($input.val() != defaultValue);
+        };
 
-    this.validate = function () {
-      return {
-        valid: true,
-        msg: null
-      };
-    };
+        this.validate = function () {
+            return {
+                valid: true,
+                msg: null
+            };
+        };
 
-    this.init();
-  }
+        this.init();
+    }
+
+    /*
+     * An example of a "detached" editor.
+     * The UI is added onto document BODY and .position(), .show() and .hide() are implemented.
+     * KeyDown events are also handled to provide handling for Tab, Shift-Tab, Esc and Ctrl-Enter.
+     */
+    function LongSimpleHtmlTextEditor(args) {
+        var $input, $wrapper, _incomingHtml, _innerText, _outgoingHtml, _toReplace = "{placeHolder}";
+        var defaultValue;
+        var scope = this;
+
+        this.init = function () {
+            var $container = $("body");
+
+            $wrapper = $("<DIV style='z-index:10000;position:absolute;background:white;padding:5px;border:3px solid gray; -moz-border-radius:10px; border-radius:10px;'/>")
+                .appendTo($container);
+
+            $input = $("<TEXTAREA hidefocus rows=5 style='backround:white;width:250px;height:80px;border:0;outline:0'>")
+                .appendTo($wrapper);
+
+            $("<DIV style='text-align:right'><BUTTON>Save</BUTTON><BUTTON>Cancel</BUTTON></DIV>")
+                .appendTo($wrapper);
+
+            $wrapper.find("button:first").bind("click", this.save);
+            $wrapper.find("button:last").bind("click", this.cancel);
+            $input.bind("keydown", this.handleKeyDown);
+
+            scope.position(args.position);
+            $input.focus().select();
+        };
+
+        this.handleKeyDown = function (e) {
+            if (e.which == $.ui.keyCode.ENTER && e.ctrlKey) {
+                scope.save();
+            } else if (e.which == $.ui.keyCode.ESCAPE) {
+                e.preventDefault();
+                scope.cancel();
+            } else if (e.which == $.ui.keyCode.TAB && e.shiftKey) {
+                e.preventDefault();
+                args.grid.navigatePrev();
+            } else if (e.which == $.ui.keyCode.TAB) {
+                e.preventDefault();
+                args.grid.navigateNext();
+            }
+        };
+
+        this.save = function () {
+            $input.val(_outgoingHtml.replace(_toReplace, $input.val()));
+            args.commitChanges();
+        };
+
+        this.cancel = function () {
+            $input.val(defaultValue);
+            args.cancelChanges();
+        };
+
+        this.hide = function () {
+            $wrapper.hide();
+        };
+
+        this.show = function () {
+            $wrapper.show();
+        };
+
+        this.position = function (position) {
+            $wrapper
+                .css("top", position.top - 5)
+                .css("left", position.left - 5)
+        };
+
+        this.destroy = function () {
+            $wrapper.remove();
+        };
+
+        this.focus = function () {
+            $input.focus();
+        };
+
+        this.loadValue = function (item) {
+            _incomingHtml = $(item[args.column.field])[0].outerHTML;
+            _innerText = $(item[args.column.field])[0].innerHTML;
+            _outgoingHtml = _incomingHtml.replace(_innerText, _toReplace);
+            $input.val(defaultValue = _innerText);
+            $input.select();
+        };
+
+        this.serializeValue = function () {
+            return $input.val();
+        };
+
+        this.applyValue = function (item, state) {
+            item[args.column.field] = state;
+        };
+
+        this.isValueChanged = function () {
+            return (!($input.val() == "" && defaultValue == null)) && ($input.val() != defaultValue);
+        };
+
+        this.validate = function () {
+            return {
+                valid: true,
+                msg: null
+            };
+        };
+
+        this.init();
+    }
 })(jQuery);
